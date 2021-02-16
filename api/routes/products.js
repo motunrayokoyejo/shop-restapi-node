8bbb94d24fb804e8758edc62049a95384a4be29b
Motunrayo Koyejo <koyejomotunrayo@gmail.com>
2021-02-16 22:07:06 +0100

    created routes

---
+++ api/routes/products.js
@@ -0,0 +1,49 @@
+const express = require('express')
+const router = express.Router();
+
+//Handle incoming GET request to /orders
+router.get('/',(req, res, next) => {
+    res.status(200).json({
+        message: 'Handling GET requests to /products'
+    });
+});
+
+router.post('/',(req, res, next) => {
+    const product = {
+        name: req.body.name,
+        price: req.body.price
+    };
+    res.status(200).json({
+        message: 'Handling POST requests to /products',
+        createdProduct: product
+    });
+});
+
+router.get('/:productId', (req, res, next) => {
+    const id = req.params.productId;
+    if (id === 'special'){
+        res.status(200).json({
+            message: 'YOu discovered the special ID',
+            id: id
+        });
+    } else{
+        res.status(200).json({
+            message: 'You passed an ID'
+        });
+    }
+});
+
+router.patch('/:productId',(req,res,next) => {
+    res.status(200).json({
+        message: 'Updated product!'
+    });
+});
+
+router.delete('/:productId',(req,res,next) => {
+    res.status(200).json({
+        message: 'Deleted product!'
+    });
+});
+
+
+module.exports = router; 
