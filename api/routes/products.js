6b00440ff633698b05143e4df2855263d887c982
Motunrayo Koyejo <koyejomotunrayo@gmail.com>
2021-02-18 05:44:15 +0100

    added models

--- api/routes/products.js
+++ api/routes/products.js
@@ -1,36 +1,76 @@
 const express = require('express')
 const router = express.Router();
+const mongoose = require('mongoose')
+
+const Product = require('../models/product');
 
 //Handle incoming GET request to /orders
 router.get('/',(req, res, next) => {
-    res.status(200).json({
-        message: 'Handling GET requests to /products'
+    // res.status(200).json({
+    //     message: 'Handling GET requests to /products'
+    // });
+    Product.find()
+    .exec()
+    .then(docs => {
+        console.log(docs);
+        res.status(200).json(docs);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({
+            error:err
+        });
     });
 });
 
 router.post('/',(req, res, next) => {
-    const product = {
+    // const product = {
+    //     name: req.body.name,
+    //     price: req.body.price
+    // };
+    const product = new Product({
+        _id: new mongoose.Types.ObjectId(),
         name: req.body.name,
         price: req.body.price
-    };
-    res.status(200).json({
-        message: 'Handling POST requests to /products',
-        createdProduct: product
     });
+      product
+      .save()
+      .then(result => {
+          console.log(result);
+          res.status(201).json({
+            message: 'Handling POST requests to /products',
+            createdProduct: result
+        });
+      })
+      .catch(err => {
+          console.log(err);
+          res.status(500).json({
+              error:err
+          });
+        })
+    
 });
 
 router.get('/:productId', (req, res, next) => {
     const id = req.params.productId;
-    if (id === 'special'){
-        res.status(200).json({
-            message: 'YOu discovered the special ID',
-            id: id
-        });
-    } else{
-        res.status(200).json({
-            message: 'You passed an ID'
-        });
-    }
+    // if (id === 'special'){
+    //     res.status(200).json({
+    //         message: 'YOu discovered the special ID',
+    //         id: id
+    //     });
+    // } else{
+    //     res.status(200).json({
+    //         message: 'You passed an ID'
+    //     });
+    // }
+    Product.findById(id).exec().then(doc => {
+        console.log(doc);
+        res.status(200).json(doc);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({error:err});
+    });
 });
 
 router.patch('/:productId',(req,res,next) => {
@@ -40,9 +80,11 @@ router.patch('/:productId',(req,res,next) => {
 });
 
 router.delete('/:productId',(req,res,next) => {
-    res.status(200).json({
-        message: 'Deleted product!'
-    });
+    // res.status(200).json({
+    //     message: 'Deleted product!'
+    // });
+    const id = req.params.productId;
+    Product.remove({_id: id})
 });
 
 
--- app.js
+++ app.js
@@ -2,25 +2,31 @@ const express = require('express')
 const app = express();
 const morgan = require('morgan');
 const bodyParser =  require('body-parser');
+const mongoose = require('mongoose')
 
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
 
+//mongodb+srv://<username>:<password>@node-restapi.uxluz.mongodb.net/<dbname>?retryWrites=true&w=majority
+
+
+mongoose.connect('mongodb+srv://motun:motunrayo@node-restapi.uxluz.mongodb.net/testDB?retryWrites=true&w=majority',
+{
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+.then(() => console.log('MongoDB connected....'))
+.catch(err => console.log(err));
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
-app.use((req,res,next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers','*');
-    if (req.method === 'OPTIONS'){
-        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
-        return res.status(200).json({});
-    }
-});
 
+
+// 
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
+
 
 app.use((req,res,next) =>{
     const error = new Error('Not found');
